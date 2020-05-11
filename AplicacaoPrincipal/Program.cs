319776ca021e82891b2d9f919e0d766562558ec3
Vinicius Araujo <vhsj@gft.com>
2020-05-11 11:54:14 -0300

    Singleton criado

---
+++ AplicacaoPrincipal/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using static System.Console;
+using Arquitetura_De_Software_Patterns_2020_Vinicius_Araujo.Singleton;
+
+namespace Arquitetura_De_Software_Patterns_2020_Vinicius_Araujo.AplicacaoPrincipal {
+    class Program {
+        static void Main(string[] args) {
+            ExecutarMenu();
+        }
+
+        private static void ExecutarMenu() {
+            bool continuar = true;
+            do {
+                Clear();
+                Title = "Menu da aplicação";
+                WriteLine("Escolha uma das opções abaixo para executar o Design Pattern correspondente:");
+                WriteLine("1 - Singleton (Buscar próximo número de ticket)");
+                WriteLine("0 - Sair");
+
+                int opcao = RecuperaInteiro("");
+
+                switch (opcao) {
+                    case 1:
+                        BuscarPróximoNumeroDeTicket();
+                        ExibirMensagemDeRetornoAoMenu();
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        WriteLine("Opção inválida!");
+                        break;
+                }
+
+            } while (continuar);
+        }
+
+        #region Singleton
+        private static void BuscarPróximoNumeroDeTicket() {
+            var ticket = TicketNumber.GetInstance().GetNextTicket();
+            WriteLine($"Número do ticket: {ticket}"); 
+        }
+            
+        #endregion
+
+        #region Métodos de funcionamento do menu
+
+        private static void ExibirMensagemDeRetornoAoMenu() {
+            WriteLine("\n\nPressione qualquer tecla para voltar ao menu..");
+            ReadKey();
+        }
+
+        /**
+         * Método responsável por recuperar valores decimais no console e tratar possíveis tentativas de fornecimento de valores inconsistentes (strings, por exemplo).
+         * Recebe via parâmetro a descrição que ele vai apresentar ao usuário na solicitação do valor.
+         * Retorna um valor em double que o usuário informou.
+         */
+        private static double RecuperaDecimal(string descricao) {
+            double valor;
+
+            try {
+                WriteLine(descricao);
+                valor = Convert.ToDouble(ReadLine());
+            } catch (Exception) {
+                WriteLine("Valor inválido!");
+                valor = RecuperaDecimal(descricao);
+            }
+            return valor;
+        }
+
+        /**
+         * Método responsável por recuperar valores inteiros no console e tratar possíveis tentativas de fornecimento de valores inconsistentes (strings, por exemplo).
+         * Recebe via parâmetro a descrição que ele vai apresentar ao usuário na solicitação do valor.
+         * Retorna um valor em int que o usuário informou.
+         */
+        private static int RecuperaInteiro(string descricao) {
+            int valor;
+
+            try {
+                WriteLine(descricao);
+                valor = Convert.ToInt32(ReadLine());
+            } catch (Exception) {
+                WriteLine("Valor inválido!");
+                valor = RecuperaInteiro(descricao);
+            }
+            return valor;
+        }
+
+        /**
+         * Método responsável por recuperar valores de texto no console e tratar possíveis tentativas de fornecimento de valores inconsistentes (string em branco, nula ou espaço).
+         * Recebe via parâmetro a descrição que ele vai apresentar ao usuário na solicitação do valor.
+         * Retorna um valor em int que o usuário informou.
+         */
+        private static string RecuperaStringNaoVazia(string descricao) {
+            string informacao;
+
+            WriteLine(descricao);
+            informacao = ReadLine();
+            if (string.IsNullOrWhiteSpace(informacao)) {
+                WriteLine("Valor inválido!");
+                informacao = RecuperaStringNaoVazia(descricao);
+            }
+            return informacao;
+        }
+        #endregion
+    }
+}
---
+++ Singleton/TicketNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arquitetura_De_Software_Patterns_2020_Vinicius_Araujo.Singleton {
+    public class TicketNumber {
+
+        private TicketNumber() { }
+
+        private static TicketNumber _instance;
+
+        private int TicketAtual = 0;
+
+        public static TicketNumber GetInstance() {
+            if (_instance == null) {
+                _instance = new TicketNumber();
+            }
+            return _instance;
+        }
+
+        public int GetNextTicket() {
+            TicketAtual++;
+            return TicketAtual;
+        }
+    }
+}
